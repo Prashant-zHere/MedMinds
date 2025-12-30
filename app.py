14f8e75ff04103cef1c585846e0936a64ba8d8d0
Prashant_zHere <zedd.prashant@gmail.com>
2025-12-30 13:16:02 +0530

    minor changes

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def chat():
 
 if __name__ == '__main__':
     logger.info("🚀 Starting MedMinds.AI server...")
-    app.run(debug=True, port=5000)
+    app.run(host = '0.0.0.0', debug=True, port=5000)
